d496c3261a2bbfa7e396d3cd6b754f8321dec1ad
Blubber <f2005278@mvrht.com>
2016-10-16 23:40:35 +0200

    fixed headers

--- .vim/install_vundle.sh
+++ .vim/install_vundle.sh
@@ -1,6 +1,5 @@
 # dotfiles -- .vim/install_vundle.sh
 # author: johannst
-# vundle: https://github.com/VundleVim/Vundle.vim
 
 # if INSTALL_DIR changed, it must be also changed in .vim/vimrc_files/vundle.vim
 INSTALL_DIR=~/.vim/bundle
